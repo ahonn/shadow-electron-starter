abe646fbaacff075d5e5ad2584bec04acb28ae2e
ahonn <ahonn95@outlook.com>
2018-01-02 00:48:44 +0800

    feat: require electron by cljs :require

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -1,23 +1,19 @@
-(ns app.main.core)
-
-(def electron       (js/require "electron"))
-(def app            (.-app electron))
-(def browser-window (.-BrowserWindow electron))
-(def crash-reporter (.-crashReporter electron))
+(ns app.main.core
+  (:require ["electron" :refer [app BrowserWindow crashReporter]]))
 
 (def main-window (atom nil))
 
 (defn init-browser []
-  (reset! main-window (browser-window.
+  (reset! main-window (BrowserWindow.
                         (clj->js {:width 800
                                   :height 600})))
   ; Path is relative to the compiled js file (main.js in our case)
-  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
+  (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.on @main-window "closed" #(reset! main-window nil)))
 
 (defn main []
   ; CrashReporter can just be omitted
-  (.start crash-reporter
+  (.start crashReporter
           (clj->js
             {:companyName "MyAwesomeCompany"
              :productName "MyAwesomeApp"
