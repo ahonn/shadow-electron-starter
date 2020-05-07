9cd31375391832175cc40402c171bc5ecacdddb3
ahonn <ahonn95@outlook.com>
2020-05-07 11:49:34 +0800

    feat: upgrade electron and shadow-cljs

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -1,31 +1,9 @@
 (ns app.renderer.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [clojure.string :as string :refer [split-lines]]))
-
-(def join-lines (partial string/join "\n"))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(defonce state        (atom 0))
-(defonce shell-result (atom ""))
-(defonce command      (atom ""))
-
-(defonce proc (js/require "child_process"))
-
-(defn append-to-out [out]
-  (swap! shell-result str out))
-
-(defn run-process []
-  (when-not (empty? @command)
-    (println "Running command" @command)
-    (let [[cmd & args] (string/split @command #"\s")
-          js-args (clj->js (or args []))
-          p (.spawn proc cmd js-args)]
-      (.on p "error" (comp append-to-out
-                           #(str % "\n")))
-      (.on (.-stderr p) "data" append-to-out)
-      (.on (.-stdout p) "data" append-to-out))
-    (reset! command "")))
+(defonce state (atom 0))
 
 (defn root-component []
   [:div
@@ -33,32 +11,11 @@
     [:img.electron {:src "img/electron-logo.png"}]
     [:img.cljs {:src "img/cljs-logo.svg"}]
     [:img.reagent {:src "img/reagent-logo.png"}]]
-   [:pre "Versions:"
-    [:p (str "Node     " js/process.version)]
-    [:p (str "Electron " ((js->clj js/process.versions) "electron"))]
-    [:p (str "Chromium " ((js->clj js/process.versions) "chrome"))]]
    [:button
     {:on-click #(swap! state inc)}
-    (str "Clicked " @state " times")]
-   [:div
-    [:form
-     {:on-submit (fn [^js/Event e]
-                   (.preventDefault e)
-                   (run-process))}
-     [:input#command
-      {:type :text
-       :on-change (fn [^js/Event e]
-                    (reset! command
-                            ^js/String (.-value (.-target e))))
-       :value @command
-       :placeholder "type in shell command"}]]]
-   [:pre (join-lines (take 100 (reverse (split-lines @shell-result))))]])
-
+    (str "Clicked " @state " times")]])
 
 (defn start! []
   (reagent/render
     [root-component]
     (js/document.getElementById "app-container")))
-
-(start!)
-
