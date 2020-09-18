4bea460e1815fab50eaa94221f4f0a72c63d6323
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2020-09-18 11:17:05 -0400

    Update deps; enable hot reloading

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -5,20 +5,20 @@
 
 (defn init-browser []
   (reset! main-window (BrowserWindow.
-                        (clj->js {:width 800
-                                  :height 600})))
+                       (clj->js {:width 800
+                                 :height 600})))
   ; Path is relative to the compiled js file (main.js in our case)
-  (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on @main-window "closed" #(reset! main-window nil)))
+  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
 
 (defn main []
   ; CrashReporter can just be omitted
   (.start crashReporter
           (clj->js
-            {:companyName "MyAwesomeCompany"
-             :productName "MyAwesomeApp"
-             :submitURL "https://example.com/submit-url"
-             :autoSubmit false}))
+           {:companyName "MyAwesomeCompany"
+            :productName "MyAwesomeApp"
+            :submitURL "https://example.com/submit-url"
+            :autoSubmit false}))
 
   (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
                                   (.quit app)))
